e6cabb43a15ab9dc41aa778a3ecf61906743dc71
kbang <kbang@10.0.0.210>
2023-09-08 02:58:21 -0400

    updated empty method to calculate addition

--- LearningGitAutomations/src/test/java/com/automation/git/LearningGitAutomation.java
+++ LearningGitAutomations/src/test/java/com/automation/git/LearningGitAutomation.java
@@ -3,9 +3,18 @@ package com.automation.git;
 public class LearningGitAutomation {
 public static void main(String[] args) {
 	System.out.println("My first git automation");
+	
+	LearningGitAutomation  myObject = new LearningGitAutomation();
+	myObject.addition(10, 40);
 }
 
 public void emptyMethod() {
 	
+}
+
+public void addition(int num1, int num2) {
+	int result = num1 + num2;
+	System.out.println("num1 is: " + num1 + ",num2 is: " + num2);
+	System.out.println("num1 + num2" + result);
 }
 }
